5bd0869a9edc79a453cbda8ae8491336cbe9c66d
JanKrzemien <jan.krzemien.6@gmail.com>
2025-01-02 22:52:38 +0100

    added many more dependencies and started working on db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { GroupsModule } from './groups/groups.module';
+import { UsersModule } from './users/users.module';
+import { ThreadsModule } from './threads/threads.module';
+import { RecommendationsModule } from './recommendations/recommendations.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProductsModule, GroupsModule, UsersModule, ThreadsModule, RecommendationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
