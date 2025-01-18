65af5957f8af25f80af39ce7ac20ac1ebf6114fc
02franek <127611343+02franek@users.noreply.github.com>
2025-01-18 19:35:02 +0100

    fix sidebar on user page

--- react-app/app/shared-components/SidebarMenu.jsx
+++ react-app/app/shared-components/SidebarMenu.jsx
@@ -1,7 +1,7 @@
-import { Stack } from '@mui/material';
+import { Stack, ButtonGroup } from '@mui/material';
 import { useNavigate } from 'react-router';
 import { useLocalStorage } from '@uidotdev/usehooks';
-import { ContainedButton } from './Buttons'; 
+import { ContainedButton, OutlineButton } from './Buttons'; 
 import './SidebarMenu.css';
 
 export const SidebarMenu = () => {
@@ -15,6 +15,17 @@ export const SidebarMenu = () => {
   }
 
   return (
+    <ButtonGroup orientation='vertical' className='sidebar-menu'>
+      <ContainedButton text="Biblioteka" onClick={() => {}} className='sidebar-btn'/>
+      <ContainedButton text="Grupy" onClick={() => {}}/>
+      <ContainedButton text="Cele" onClick={() => {}}/>
+      <ContainedButton text="Ustawienia" onClick={() => {}}/>
+      <ContainedButton text="Wyloguj się" onClick={logOut}/>
+    </ButtonGroup>
+  );
+
+  /*
+    return (
     <Stack direction="row" spacing={2} className='sidebar-menu'>
       <ContainedButton text="Biblioteka" onClick={() => {}}/>
       <ContainedButton text="Grupy" onClick={() => {}}/>
@@ -23,4 +34,5 @@ export const SidebarMenu = () => {
       <ContainedButton text="Wyloguj się" onClick={logOut}/>
     </Stack>
   );
+  */
 };
